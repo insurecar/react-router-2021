81fd12e04951c25f1ce4f20dd85bcd5e63cdd751
ostaoleho <oleh.ostashevskyi@otakoyi.com.ua>
2021-09-22 18:54:10 +0300

    finished 30.25 seconds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+
 function App() {
-  return <div className="App">fsdfsdf</div>;
+  return (
+    <div className="App">
+      <Counter />
+      <ClassCounter />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
